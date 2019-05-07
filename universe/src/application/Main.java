82a91e464499b0e9728228e4c15ff11ca0ba1e6d
eunsoo kim <wooju066@gamile.com>
2019-05-07 13:26:28 +0900

    ddd

--- universe/src/application/Main.java
+++ universe/src/application/Main.java
@@ -15,6 +15,8 @@ public class Main extends Application {
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
+			
+			//5555
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
