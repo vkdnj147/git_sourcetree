63d78e1b8689766ca6b019121343507c411a8367
eunsoo kim <wooju066@gamile.com>
2019-05-07 16:19:19 +0900

    현재 LessonVo 완료

---
+++ universe/src/model/LessonVO.java
@@ -0,0 +1,53 @@
+package model;
+
+public class LessonVO {
+
+	int no ; //일련번호
+	String l_num;
+	String l_name;
+	
+	//디폴트 생성자
+	public LessonVO() {
+		super();
+	}
+
+	public LessonVO(String l_num, String l_name) {
+		super();
+		this.l_num = l_num;
+		this.l_name = l_name;
+	}
+
+	public LessonVO(int no, String l_num, String l_name) {
+		super();
+		this.no = no;
+		this.l_num = l_num;
+		this.l_name = l_name;
+	}
+
+	public int getNo() {
+		return no;
+	}
+
+	public void setNo(int no) {
+		this.no = no;
+	}
+
+	public String getL_num() {
+		return l_num;
+	}
+
+	public void setL_num(String l_num) {
+		this.l_num = l_num;
+	}
+
+	public String getL_name() {
+		return l_name;
+	}
+
+	public void setL_name(String l_name) {
+		this.l_name = l_name;
+	}
+
+	
+	
+}
