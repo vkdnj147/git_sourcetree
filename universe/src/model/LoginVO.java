b1f6dcad431abcefeba3cab9f588b3b78c4039fa
su hyeon kim <vkdnj4147@gmail.com>
2019-05-07 13:42:28 +0900

    로그인 VO 생성

---
+++ universe/src/model/LoginVO.java
@@ -0,0 +1,22 @@
+package model;
+
+public class LoginVO {
+	private String id;
+
+	public LoginVO() {
+		super();
+	}
+
+	public LoginVO(String id) {
+		super();
+		this.id = id;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+}
