0c5651639062208c2760159fe3b1e726dbbaa576
su hyeon kim <vkdnj4147@gmail.com>
2019-05-07 13:34:49 +0900

    dbutil 까지 생성

--- universe/src/application/Main.java
+++ universe/src/application/Main.java
@@ -1,7 +1,9 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 
@@ -10,13 +12,14 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
+			Parent root  = FXMLLoader.load(getClass().getResource("/view/login.fxml"));
+	         Scene scene = new Scene(root);
+	         primaryStage.setTitle("미래 대학교 수강신청");
+	         primaryStage.setScene(scene);
+	         primaryStage.setResizable(false);
+	         primaryStage.show();
 			
-			//5555
+			
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
---
+++ universe/src/control/DBUtil.java
@@ -0,0 +1,16 @@
+package control;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBUtil {
+   static final String driver = "oracle.jdbc.driver.OracleDriver";
+   static final String url = "jdbc:oracle:thin:@127.0.0.1:1521:orcl";
+   
+   public static Connection getConnection()throws Exception{
+      Class.forName(driver);
+      Connection con = DriverManager.getConnection(url,"scott","tiger");
+      System.out.println("DB 연결 성공");
+      return con;
+   }
+}
