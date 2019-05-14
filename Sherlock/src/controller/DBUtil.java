18be96b53a0d0909c50876128e4ad29d7a7fb203
su hyeon kim <vkdnj4147@gmail.com>
2019-05-14 09:31:03 +0900

    dbutil 생성

---
+++ Sherlock/src/controller/DBUtil.java
@@ -0,0 +1,17 @@
+package controller;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBUtil {
+	static final String driver = "oracle.jdbc.driver.OracleDriver";
+	   static final String url = "jdbc:oracle:thin:@192.168.0.115:1521:orcl";
+	   
+	   public static Connection getConnection()throws Exception{
+	      Class.forName(driver);
+	      Connection con = DriverManager.getConnection(url,"scott","tiger");
+	      System.out.println("DB 연결 성공");
+	      return con;
+	   }
+	}
+
