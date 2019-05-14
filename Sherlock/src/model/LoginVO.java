3071214b4bba105ef3e75d6a9d308ca9411628b4
eunsoo kim <wooju066@gamile.com>
2019-05-14 09:51:02 +0900

    로그인 vo 작성 완료

--- Sherlock/src/model/LoginVO.java
+++ Sherlock/src/model/LoginVO.java
@@ -2,4 +2,28 @@ package model;
 
 public class LoginVO {
 
+	private String id ; //아이디
+
+	//디폴드 생성자
+	public LoginVO() {
+		super();
+	}
+
+	//생성자
+	public LoginVO(String id) {
+		super();
+		this.id = id;
+	}
+
+	//접근자와 설정자
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	
+	
 }
